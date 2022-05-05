416a0fe975efe96300cfee0e3cba65c721180384
Evgeniy Gazetdinov <e.gazetdinov@litebox.ru>
2022-05-05 08:24:41 +0300

    replace first try

---
+++ elem/replace_first.py
@@ -0,0 +1,20 @@
+from typing import Iterable
+
+
+def replace_first(items: list) -> Iterable:
+    first = items.index(0)
+    last = items.index(len(items))
+    last2 = items.pop()
+    items.insert(0, last2)
+    return items
+
+
+if __name__ == "__main__":
+    print("Example:")
+    print(list(replace_first([1, 2, 3, 4])))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(replace_first([1, 2, 3, 4])) == [2, 3, 4, 1]
+    assert list(replace_first([1])) == [1]
+    assert list(replace_first([])) == []
+    print("Coding complete? Click 'Check' to earn cool rewards!")
