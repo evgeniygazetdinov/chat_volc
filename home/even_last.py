0c1c209b608321fed6295fdbbae50a8eb6537604
Евгений Газетдинов <e.gazetdinov@litebox.ru>
2022-10-10 07:03:43 +0300

    add data file TODO fix it

--- home/even_last.py
+++ home/even_last.py
@@ -1,16 +1,24 @@
-def checkio(array: list[int]) -> int:
-    if not len(array):
-        return 0
-    else:
-        
+RIGHT = 'right'
+LEFT = 'left'
 
+def left_join(phrases: tuple) -> str:
+    """
+    Join strings and replace "right" to "left"
+    """
+    def check_each_word(word: str):
+        new_word = word
+        if RIGHT in word:
+            new_word = word.replace(RIGHT, LEFT)
+        return new_word
+    res = [check_each_word(word) for word in phrases]
+    return ",".join(res)
 
 print("Example:")
-print(checkio([0, 1, 2, 3, 4, 5]))
+print(left_join(("left", "right", "left", "stop")))
 
-assert checkio([0, 1, 2, 3, 4, 5]) == 30
-assert checkio([1, 3, 5]) == 30
-assert checkio([6]) == 36
-assert checkio([]) == 0
+assert left_join(("left", "right", "left", "stop")) == "left,left,left,stop"
+assert left_join(("bright aright", "ok")) == "bleft aleft,ok"
+assert left_join(("brightness wright",)) == "bleftness wleft"
+assert left_join(("enough", "jokes")) == "enough,jokes"
 
 print("The mission is done! Click 'Check Solution' to earn rewards!")
